84d24fe03138ce002621356a4ce904a866848ae5
creativecoderr <creativecoderr@gmail.com>
2020-07-06 00:11:20 +0530

    -- adding date

--- src/FeedCardComponent/FeedCardComponent.js
+++ src/FeedCardComponent/FeedCardComponent.js
@@ -2,27 +2,25 @@
 import React from 'react';
 
 class FeedCard extends React.Component {
-  constructor(props) {
-    super(props);
-  }
 
   render() {
     // console.log("this.props", this.props)
 
     let data = this.props.feedData === undefined || this.props.feedData === "" ? "" : this.props.feedData;
 
-    console.log("data inside component: ")
-    console.log(data)
+    // console.log("data inside component: ")
+    // console.log(data)
 
     return (
       <div className="feed">
         <p>UserID : {data.id}</p>
         <img src={data.thumbnail_image} alt="thumbnail_photo" height="100px" width="100px"></img>
         <p>Event Name: {data.event_name}</p>
-        <p>Date: {data.event_date}</p>
+        <p>Date: {Date(data.event_date).slice(0, 15)}</p>
         <p>Views: {data.views}</p>
         <p>Likes: {data.likes}</p>
         <p>Shares: {data.shares}</p>
+        <hr></hr>
       </div>
     )
   }
